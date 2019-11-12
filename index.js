fb26a09353361681ab0d5cbcccb3183a2943395d
sandra <sanesga@gmail.com>
2019-11-12 17:06:52 +0100

    Finalizado registro y login

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ var inputLoginName = document.getElementById("inputLoginName");
 var inputLoginPassword = document.getElementById("inputLoginPassword");
 var inputRegistroName = document.getElementById("inputRegistroName");
 var inputRegistroPassword = document.getElementById("inputRegistroPassword");
-var inputRegistroPasswordRepeat = document.getElementById("inputRegistroPasswordRepeat");
+var inputRegistroPasswordRepeat = document.getElementById(
+  "inputRegistroPasswordRepeat"
+);
 var nombreUsuario = document.getElementById("nombreUsuario");
 var botonEntrarLogin = document.getElementById("botonEntrarLogin");
 var botonEntrarRegistro = document.getElementById("botonEntrarRegistro");
@@ -41,7 +43,8 @@ var monedas = 0;
 var contadorLetras = 0;
 var pista = false;
 var usuarios = new Array();
-var usuarioActual =new Object();
+var usuarioActual = new Object();
+var usuarioExiste=false;
 
 //listeners
 botonClasico.addEventListener("click", jugarClasico);
@@ -61,7 +64,7 @@ logOut.addEventListener("click", cerrarSesion);
 
 /////////////////////////////////////////////////////////////////////////////
 
-//ocultamos el menú categoría
+//ocultamos el menú categoríacerrarSesion
 divElegirCategoria.style.display = "none";
 divPalabra.style.display = "none";
 divDibujo.style.display = "none";
@@ -69,19 +72,31 @@ divMensajeFinal.style.display = "none";
 formularioLogin.style.display = "none";
 formularioRegistro.style.display = "none";
 
+//obtenemos los datos del local storage
+leerDatos();
+
 function reiniciarPagina() {
   location.reload();
 }
-function cerrarSesion(){
+function cerrarSesion() {
   //al cerrar la pestaña del navegador, guardamos el usuario actual con sus atributos modificados en el array de usuarios y en el local storage
-  for (usuario of usuarios){
-    if(usuarioActual.name==usuario.name){
-      usuario.monedas=usuarioActual.monedas;
+  for (usuario of usuarios) {
+    if (usuarioActual.name == usuario.name) { //la clave primaria del objecto usuario es el name, solo podemos modificar las monedas
+      usuario.monedas = usuarioActual.monedas;
     }
   }
-  console.log(usuarios);
+  // usuarios=[];
+guardarUsuarios();
+
+//quitamos el nombre del usuario y el logout del menú
+nombreUsuario.innerHTML="usuario";
+pMonedas.style.display="none";
+}
+//nada más entrar a la página cargamos el array de usuarios para trabajar con él
+function leerDatos() {
   if (typeof Storage !== "undefined") {
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    let json = localStorage.getItem("usuarios");
+    usuarios = JSON.parse(json);
   } else {
     alert("tu navegador no soporta el guardado de datos, prueba con otro.");
   }
@@ -102,39 +117,45 @@ function mostrarRegistro() {
   inputRegistroName.focus();
 }
 
+//entra aquí al darle al botón registrar del formulario de registro
 function registrarse() {
   //recogemos los datos del formulario y registramos al usuario
   let registroNombre = inputRegistroName.value;
   let registroPassword = inputRegistroPassword.value;
   let registroPassword2 = inputRegistroPasswordRepeat.value;
-
   //creamos una clase usuario
   let usuario = new Object();
   usuario.name = registroNombre;
   usuario.password = registroPassword;
   usuario.monedas = 0;
-
+  //añadimos el usuario al array de usuarios
   usuarios.push(usuario);
-
   //guardamos en local storage (la información no se eliminará nunca, si no la borramos nosotros)
+  guardarUsuarios();
+  //una vez registrado, vamos a la pantalla de login
+  formularioRegistro.style.display = "none";
+  formularioLogin.style.display = "block";
+  inputLoginName.focus();
+}
+function guardarUsuarios() {
   if (typeof Storage !== "undefined") {
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
   } else {
     alert("tu navegador no soporta el guardado de datos, prueba con otro.");
   }
-  formularioRegistro.style.display = "none";
-  formularioLogin.style.display = "block";
 }
-
+//entra aquí al hacer click en el botón entrar del formulario de login
 function login() {
-  formularioLogin.style.display = "none";
-  divElegirCategoria.style.display = "block";
+  
+  existeUsuario=false;
+
+  divElegirCategoria.style.display = "none";
   //recogemos los datos
   let loginName = inputLoginName.value;
   let loginPassword = inputLoginPassword.value;
 
-  usuarioActual.name = loginName;
-  usuarioActual.password = loginPassword;
+  // console.log(loginName);
+  // console.log(loginPassword);
 
   //verificamos que el usuario esté registrado en la base de datos
   //recuperamos datos
@@ -142,14 +163,23 @@ function login() {
   usuarios = JSON.parse(json);
 
   for (usuario of usuarios) {
-    console.log(usuario.name);
-    console.log(usuarioActual.name);
-    if (usuarioActual.name == usuario.name) {
-      nombreUsuario.innerHTML=usuario.name;
-    } else {
-      alert("el usuario no existe, debes registrarte");
+  // console.log(usuario.name);
+  // console.log(usuario.password);
+    if (loginName == usuario.name && loginPassword == usuario.password) {
+      usuarioActual=usuario;
+      nombreUsuario.innerHTML = usuarioActual.name;
+      pMonedas.innerHTML="<img src='./img/coin.png' alt='moneda'> x "+ usuarioActual.monedas;
+      existeUsuario = true;
     }
   }
+  if(!existeUsuario){
+    alert("El usuario no existe");
+  }
+
+  //volvemos a la pantalla principal
+  formularioLogin.style.display = "none";
+  botonClasico.style.display = "block";
+  botonAventura.style.display = "block";
 }
 
 function darPista() {
@@ -158,8 +188,23 @@ function darPista() {
       //este span estará vacío, insertamos la pista
       spans[i].style.border = "none"; //quitamos el borde que delimita el hueco
       spans[i].innerHTML = palabra[i]; //la letra que falta en ese hueco está en la misma posición que la posición del span vacío
-      pista = true; //ya hemos dado la pista
       contadorLetras++;
+
+      // if (contadorLetras == palabra.length) {
+      //   inputLetra.disabled = true;
+      //   setTimeout(function() {
+      //     //si tenemos 10 fallos, se acaba el juego
+      //     divMensajeFinal.style.display = "block";
+      //     divPalabra.style.display = "none";
+      //     divDibujo.style.display = "none";
+      //     pTextoFinal.innerHTML = "Has ganado";
+      //     contadorLetras = 0;
+      //     spans = [];
+      //   }, 3000);
+      // }
+
+      pista = true; //ya hemos dado la pista
+     
       inputLetra.focus();
     }
   }
@@ -175,82 +220,81 @@ function jugarClasico() {
   divElegirCategoria.style.display = "block";
 }
 
-
 function verificarLetra(event) {
-    //si el código de la tecla es el del intro
-    if (event.keyCode == 13) {
-      //recogemos la letra del input
-      let texto = inputLetra.value;
-      //pasamos la letra a minúscula por si no lo estuviera
-      letra = texto.toLowerCase();
-      //verificamos si la letra se encuentra en la palabra
-  let numeroLetras = 0;
+  //si el código de la tecla es el del intro
+  if (event.keyCode == 13) {
+    //recogemos la letra del input
+    let texto = inputLetra.value;
+    //pasamos la letra a minúscula por si no lo estuviera
+    letra = texto.toLowerCase();
+    //verificamos si la letra se encuentra en la palabra
+    let numeroLetras = 0;
 
-  //nos recorremos la palabra
-  for (var i = 0; i < palabra.length; i++) {
-    //si la letra pulsada está en ella
-    if (letra == palabra.charAt(i)) {
-      //pintamos la letra
-      pintarLetra(letra, i);
-      //contador para mostrar el número de letras encontradas en el mensaje
-      numeroLetras++;
-      contadorLetras++;
+    //nos recorremos la palabra
+    for (var i = 0; i < palabra.length; i++) {
+      //si la letra pulsada está en ella
+      if (letra == palabra.charAt(i)) {
+        //pintamos la letra
+        pintarLetra(letra, i);
+        //contador para mostrar el número de letras encontradas en el mensaje
+        numeroLetras++;
+        contadorLetras++;
 
-      //sumamos 5 monedas por letra acertada
-      monedas += 5;
-      //guardamos las monedas en el usuario
-      usuarioActual.monedas=monedas;
+        //sumamos 5 monedas por letra acertada
+        monedas += 5;
+        //guardamos las monedas en el usuario
+        usuarioActual.monedas = monedas;
 
-      if (contadorLetras == palabra.length) {
+        if (contadorLetras == palabra.length) {
+          inputLetra.disabled = true;
+          setTimeout(function() {
+            //si tenemos 10 fallos, se acaba el juego
+            divMensajeFinal.style.display = "block";
+            divPalabra.style.display = "none";
+            divDibujo.style.display = "none";
+            pTextoFinal.innerHTML = "Has ganado";
+            contadorLetras = 0;
+            spans = [];
+          }, 3000);
+        }
+      }
+    }
+    //imprimimos las monedas en el p
+    pMonedas.innerHTML = "<img src='./img/coin.png' alt='moneda'> x " + monedas;
+    if (numeroLetras == 1) {
+      pMensajes.innerHTML =
+        "La letra se encuentra " + numeroLetras + " vez en la palabra";
+    } else if (numeroLetras > 1) {
+      pMensajes.innerHTML =
+        "La letra se encuentra " + numeroLetras + " veces en la palabra";
+    }
+    if (numeroLetras == 0) {
+      //si la letra no está
+      pMensajes.innerHTML = "Esta letra no se encuentra en la palabra";
+      inputLetra.value = "";
+      inputLetra.focus();
+      divDibujo.style.display = "block";
+      divDibujo.addEventListener("change", transicion);
+      divDibujo.style.backgroundImage =
+        "url('./img/fallo" + contadorFallos + ".png')";
+      contadorFallos++;
+
+      pFallos.innerHTML = "FALLO " + contadorFallos + " / 10";
+
+      if (contadorFallos == 10) {
         inputLetra.disabled = true;
         setTimeout(function() {
           //si tenemos 10 fallos, se acaba el juego
           divMensajeFinal.style.display = "block";
           divPalabra.style.display = "none";
           divDibujo.style.display = "none";
-          pTextoFinal.innerHTML = "Has ganado";
+          pTextoFinal.innerHTML = "Has perdido";
           contadorLetras = 0;
           spans = [];
         }, 3000);
       }
     }
   }
-  //imprimimos las monedas en el p
-  pMonedas.innerHTML = "<img src='./img/coin.png' alt='moneda'> x " + monedas;
-  if (numeroLetras == 1) {
-    pMensajes.innerHTML =
-      "La letra se encuentra " + numeroLetras + " vez en la palabra";
-  } else if (numeroLetras > 1) {
-    pMensajes.innerHTML =
-      "La letra se encuentra " + numeroLetras + " veces en la palabra";
-  }
-  if (numeroLetras == 0) {
-    //si la letra no está
-    pMensajes.innerHTML = "Esta letra no se encuentra en la palabra";
-    inputLetra.value = "";
-    inputLetra.focus();
-    divDibujo.style.display = "block";
-    divDibujo.addEventListener("change", transicion);
-    divDibujo.style.backgroundImage =
-      "url('./img/fallo" + contadorFallos + ".png')";
-    contadorFallos++;
-
-    pFallos.innerHTML = "FALLO " + contadorFallos + " / 10";
-
-    if (contadorFallos == 10) {
-      inputLetra.disabled = true;
-      setTimeout(function() {
-        //si tenemos 10 fallos, se acaba el juego
-        divMensajeFinal.style.display = "block";
-        divPalabra.style.display = "none";
-        divDibujo.style.display = "none";
-        pTextoFinal.innerHTML = "Has perdido";
-        contadorLetras = 0;
-        spans = [];
-      }, 3000);
-    }
-  }
-}
 }
 
 function transicion() {
